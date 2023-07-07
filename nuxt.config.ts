54609175add225a7d6bcd58666048ca985d1df68
Adam Blahovič <blahovic.adam@icloud.com>
2023-07-07 15:57:09 +0200

    initialize project

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            "midnight-blue": {
+              "50": "#f4f6fe",
+              "100": "#eaedfd",
+              "200": "#d9dffb",
+              "300": "#bac3f8",
+              "400": "#929cf3",
+              "500": "#666cec",
+              "600": "#4845e2",
+              "700": "#3833ce",
+              "800": "#2f2aad",
+              "900": "#29258d",
+              "950": "#191970",
+            },
+          },
+        },
+      },
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex w-screen h-screen items-center justify-center">
+    <h1 class="text-6xl">
+      Semanti<span class="text-midnight-blue-900">/</span>core
+    </h1>
+  </div>
+</template>
