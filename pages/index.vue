5adfc98a0316585791d07f3dfc780dceeed80ce6
Adam Blahovič <blahovic.adam@icloud.com>
2023-07-09 12:39:28 +0200

    use otp for sign in instead of email/password

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,27 @@
     <h1 class="text-6xl select-none p-4">
       Semanti<span class="text-midnight-blue-900">/</span>core
     </h1>
+
     <div
       class="border border-midnight-blue-800 border-2 bg-midnight-blue-50 rounded-md border-slate-950 px-8 py-4 flex flex-col items-center gap-4"
     >
-      <h1 class="text-2xl font-semibold">
-        Sign <span class="text-midnight-blue-900">in</span>
-      </h1>
       <Button
         variant="outlined"
         @click="auth.signInWithOAuth({ provider: 'github' })"
         ><Icon name="uil:github" class="h-5 w-5" />Sign in via GitHub
       </Button>
-      <hr class="h-0.5 w-56 border-0 bg-midnight-blue-900" />
-      <form
-        @submit.prevent="passwordLogin"
-        class="flex flex-col gap-2 items-center"
-      >
+
+      <div class="flex items-center gap-2">
+        <hr class="h-0.5 w-32 border-0 bg-midnight-blue-900" />
+        <p>or</p>
+        <hr class="h-0.5 w-32 border-0 bg-midnight-blue-900" />
+      </div>
+
+      <h1 class="text-xl font-semibold">
+        Sign in<span class="text-midnight-blue-900">/</span>up using just email
+      </h1>
+
+      <form @submit.prevent="otpLogin" class="flex flex-col gap-2 items-center">
         <Input
           class="w-72"
           autocomplete="email"
@@ -29,25 +34,12 @@
           v-model="email"
         />
 
-        <Input
-          class="w-72"
-          autocomplete="current-password"
-          name="password"
-          placeholder="Password"
-          type="password"
-          required="true"
-          v-model="password"
-        />
+        <p class="text-midnight-blue-900 text-sm">{{ infoMessage }}</p>
         <p class="text-red-500 text-sm">{{ errorMessage }}</p>
+
         <Button type="submit">Sign in</Button>
       </form>
     </div>
-    <p>
-      Don't have an account yet?
-      <span class="text-midnight-blue-900">
-        <NuxtLink to="register">Register</NuxtLink></span
-      >
-    </p>
   </div>
 </template>
 
@@ -55,6 +47,7 @@
 definePageMeta({
   layout: "intro",
 });
+
 const user = useSupabaseUser();
 const { auth } = useSupabaseAuthClient();
 
@@ -65,19 +58,20 @@ watchEffect(() => {
 });
 
 const email = ref("");
-const password = ref("");
 const errorMessage = ref("");
+const infoMessage = ref("");
 
-const passwordLogin = async () => {
-  const { error } = await auth.signInWithPassword({
+const otpLogin = async () => {
+  const { error } = await auth.signInWithOtp({
     email: email.value,
-    password: password.value,
   });
 
   if (error) {
     errorMessage.value = error.message;
+    infoMessage.value = "";
   } else {
-    navigateTo("/dashboard");
+    infoMessage.value = "Check your email for the magic link";
+    errorMessage.value = "";
   }
 };
 </script>
