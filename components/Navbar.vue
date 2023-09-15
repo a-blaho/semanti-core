48c1d3161bfd2152ed2dabb38267ba70967d32be
Adam Blahovič <blahovic.adam@icloud.com>
2023-09-15 16:50:57 +0200

    change navbar logo color on hover

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,14 @@
 <template>
   <div
-    class="flex flex-col fixed top-0 left-0 h-screen w-48 bg-midnight-blue-950 items-center justify-between py-8 text-white"
+    class="flex flex-col fixed top-0 left-0 h-screen w-48 bg-midnight-blue-900 items-center justify-between py-8 text-white"
   >
-    <NuxtLink to="/dashboard">semanti/core</NuxtLink>
+    <div @mouseover="hover = true" @mouseout="hover = false">
+      <NuxtLink to="/dashboard" class="text-xl"
+        >semanti
+        <span :class="{ 'text-midnight-blue-500': hover }">/</span>
+        core</NuxtLink
+      >
+    </div>
     <div class="flex flex-col gap-2">
       <NuxtLink to="/new-dataset">New dataset</NuxtLink>
       <br />
@@ -16,4 +22,6 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const hover = ref(false);
+</script>
