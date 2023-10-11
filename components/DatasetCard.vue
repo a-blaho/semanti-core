e6b4fca89ea77971d8d2f972cfafd37e20686f9d
Adam Blahovič <adam.blahovic@icloud.com>
2023-10-11 14:24:07 +0200

    center dataset cards

--- components/DatasetCard.vue
+++ components/DatasetCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     @click="navigateTo('/datasets/' + dataset?.id)"
-    class="p-4 bg-midnight-200 border rounded-md h-48 w-72 cursor-pointer hover:bg-midnight-300"
+    class="p-4 bg-midnight-200 border rounded-md h-48 w-72 cursor-pointer hover:bg-midnight-300 justify-self-center align-self-center"
     :class="{ 'animate-pulse': loading }"
   >
     <template v-if="!loading && dataset">
