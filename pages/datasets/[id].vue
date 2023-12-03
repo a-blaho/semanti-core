adbf108af8cea065c619096d83a7fdb4b542593b
Adam Blahovič <adam.blahovic@icloud.com>
2023-12-03 13:18:02 +0100

    allow users to download metadata

--- pages/datasets/[id].vue
+++ pages/datasets/[id].vue
@@ -23,9 +23,6 @@
       </div>
       <div>
         <DatasetStar :dataset-id="dataset.id" />
-        <Button @click="navigateTo(downloadUrl, { external: true })"
-          >Download CSV</Button
-        >
       </div>
     </div>
     <div style="grid-area: main" class="overflow-auto">
@@ -42,7 +39,14 @@
               <h2 class="text-xl font-bold">Description</h2>
               <p class="text-lg">{{ dataset.metadata["dc:description"] }}</p>
             </div>
-
+            <div class="flex justify-between gap-16">
+              <Button class="w-full" @click="downloadDataset()">
+                Download CSV
+              </Button>
+              <Button class="w-full" @click="downloadMetadata()"
+                >Download metadata</Button
+              >
+            </div>
             <div>
               <h2 class="text-xl font-bold">Charts</h2>
               <div
@@ -95,7 +99,6 @@ definePageMeta({
 const client = useSupabaseClient<Database>();
 const route = useRoute();
 
-const downloadUrl = ref<string | null>(null);
 const dataset = ref<null | {
   id: string;
   metadata: Metadata;
@@ -106,15 +109,38 @@ const rows = ref<any>([]);
 const tabStyle =
   "border-b px-8 ui-selected:border-space-950 focus:outline-none";
 
+const downloadMetadata = async () => {
+  const metadata = dataset.value?.metadata;
+  if (!metadata) return;
+
+  const blob = new Blob([JSON.stringify(metadata)], {
+    type: "application/json",
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `${route.params.id}-metadata.json`;
+  a.click();
+  URL.revokeObjectURL(url);
+};
+
+const downloadDataset = async () => {
+  const { data: url } = await client.storage
+    .from("datasets")
+    .createSignedUrl(`${route.params.id}/${route.params.id}.csv`, 60);
+
+  if (!url) return;
+
+  navigateTo(url.signedUrl, { external: true });
+};
+
 onMounted(async () => {
-  const [{ data }, { data: url }, { data: file }] = await Promise.all([
+  const [{ data }, { data: datasetFile }] = await Promise.all([
     client
       .from("datasets")
       .select("id, owner (name), metadata")
       .eq("id", route.params.id),
-    client.storage
-      .from("datasets")
-      .createSignedUrl(`${route.params.id}/${route.params.id}.csv`, 60),
+
     client.storage
       .from("datasets")
       .download(`${route.params.id}/${route.params.id}.csv`),
@@ -130,11 +156,11 @@ onMounted(async () => {
       },
     };
   }
-  if (file) {
-    rows.value = await parseCsv({ file, options: { start: 1, end: 11 } });
-    console.log(rows.value);
+  if (datasetFile) {
+    rows.value = await parseCsv({
+      file: datasetFile,
+      options: { start: 1, end: 11 },
+    });
   }
-
-  downloadUrl.value = url?.signedUrl ?? null;
 });
 </script>
